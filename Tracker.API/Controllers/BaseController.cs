0dcda2ede6a6aa3b6956e7e70b9fb9aeea358a82
LogesuwaranP <logesuwaran005@gmail.com>
2024-05-05 10:44:17 +0530

    introduced base controller

---
+++ Tracker.API/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Tracker.API.ResponseModels;
+using Tracker.Data.Entities;
+using Tracker.Domain.Service.FinancialProductService;
+
+namespace Tracker.API.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class BaseController : Controller
+    {
+
+        [HttpGet]
+        public Task<string> GetListOfFinancialProduct()
+        {
+            return Task.FromResult("Server up in running... ");
+        }
+    }
+}
